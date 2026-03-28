b96d702c307e704cdc31b92f941da62b46fcfa49
Arsalan Mir <arsalanmir735@gmail.com>
2026-03-28 22:53:19 +0500

    Delete online classes backend module

--- clashguard-sync-api/src/onlineClasses/constants.js
+++
@@ -1,6 +0,0 @@
-export const ONLINE_CLASSES_SHEET_ID = '1x_wi07pYRGObmWDFIoFpaHXmgj2YQQhTJMMwFRVNWwA';
-export const ONLINE_CLASSES_CACHE_TTL_MS = 5 * 60 * 1000;
-export const ONLINE_CLASSES_DAY_SHEETS = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-
-export const getOnlineClassesGvizUrl = (sheetName) =>
-  `https://docs.google.com/spreadsheets/d/${ONLINE_CLASSES_SHEET_ID}/gviz/tq?tqx=out:json&sheet=${encodeURIComponent(sheetName)}`;
